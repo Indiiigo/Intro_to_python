722581599951f58a1aa671196984cec59188b655
Indira Sen <indira15021@iiitd.ac.in>
2024-02-22 11:39:38 +0100

    + added variable ex1 solutions

---
+++ day 1_ Thursday/notebooks/11_var_string_num_ex1_1.py
@@ -0,0 +1,9 @@
+# Store a message in a variable
+msg = "Hey"
+# Print that message
+print(msg)
+
+# Store a new message in the same variable
+msg = "How are you?"
+# Print that new message
+print(msg)
