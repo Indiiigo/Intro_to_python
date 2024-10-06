272f40d7a0c3e3283b271470a9f0f9b06e0ac1b1
Indira Sen <indira15021@iiitd.ac.in>
2024-10-06 22:54:50 +0200

    - deleted solutions

--- day 1_ Monday/notebooks/11_var_string_num_ex1_1.py
+++
@@ -1,9 +0,0 @@
-# Store a message in a variable
-msg = "Hey"
-# Print that message
-print(msg)
-
-# Store a new message in the same variable
-msg = "How are you?"
-# Print that new message
-print(msg)
